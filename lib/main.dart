a244dfd4233414273e1007b3493d5f921252a827
jalaprthi1 <alaparthijoshika@gmail.com>
2025-04-02 23:23:34 -0400

    changed ui

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,18 @@ class InventoryApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Inventory Management',
-      theme: ThemeData.dark().copyWith(
+      title: 'Inventory Management App',
+      theme: ThemeData(
+        brightness: Brightness.dark,
         primaryColor: Colors.tealAccent,
         scaffoldBackgroundColor: Colors.black,
-        cardColor: Colors.grey[900],
+        cardColor: Color(0xFF1C1C1E),
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white70),
+        ),
         appBarTheme: AppBarTheme(
-          backgroundColor: Colors.black,
+          backgroundColor: Color(0xFF121212),
           titleTextStyle: TextStyle(
             color: Colors.tealAccent,
             fontWeight: FontWeight.bold,
@@ -47,14 +52,25 @@ class _InventoryHomePageState extends State<InventoryHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Inventory Management'),
+        actions: [
+          PopupMenuButton(
+            icon: Icon(Icons.more_vert),
+            itemBuilder: (context) => [
+              PopupMenuItem(value: 'settings', child: Text('Settings')),
+              PopupMenuItem(value: 'help', child: Text('Help')),
+            ],
+            onSelected: (value) {},
+          ),
+        ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             Text(
               'Inventory Items',
-              style: Theme.of(context).textTheme.headline6?.copyWith(
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(
                     color: Colors.tealAccent,
                     fontWeight: FontWeight.bold,
                   ),
@@ -63,12 +79,7 @@ class _InventoryHomePageState extends State<InventoryHomePage> {
               child: StreamBuilder(
                 stream: inventory.snapshots(),
                 builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator());
-                  }
-                  if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-                    return Center(child: Text('No items found.'));
-                  }
+                  if (!snapshot.hasData) return CircularProgressIndicator();
                   return GridView.builder(
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2,
@@ -79,7 +90,47 @@ class _InventoryHomePageState extends State<InventoryHomePage> {
                     itemCount: snapshot.data!.docs.length,
                     itemBuilder: (context, index) {
                       var document = snapshot.data!.docs[index];
-                      return _buildInventoryCard(document);
+                      return Card(
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                document['name'],
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                'Quantity: ${document['quantity']}',
+                                style: TextStyle(color: Colors.white70),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  IconButton(
+                                    icon: Icon(Icons.edit,
+                                        color: Colors.tealAccent),
+                                    onPressed: () => _editItem(document),
+                                  ),
+                                  IconButton(
+                                    icon: Icon(Icons.delete,
+                                        color: Colors.redAccent),
+                                    onPressed: () => _deleteItem(document.id),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
                     },
                   );
                 },
@@ -89,87 +140,73 @@ class _InventoryHomePageState extends State<InventoryHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _showItemDialog(),
+        onPressed: _addItem,
         child: Icon(Icons.add, color: Colors.black),
       ),
+      // bottomNavigationBar: Padding(
+      //   padding: const EdgeInsets.all(16.0),
+      //   child: Card(
+      //     color: Color(0xFF1C1C1E),
+      //     shape: RoundedRectangleBorder(
+      //       borderRadius: BorderRadius.circular(8),
+      //     ),
+      //     child: Padding(
+      //       padding: const EdgeInsets.all(12.0),
+      //       child: Center(
+      //         child: Text(
+      //           'Abhiram Gelle\nPanther ID: 002843022',
+      //           textAlign: TextAlign.center,
+      //           style: TextStyle(color: Colors.white70),
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 
-  Widget _buildInventoryCard(QueryDocumentSnapshot document) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              document['name'],
-              style: TextStyle(
-                  color: const Color.fromARGB(255, 255, 255, 255),
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              'Quantity: ${document['quantity']}',
-              style: TextStyle(color: Colors.white70),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  icon: Icon(Icons.edit, color: Colors.tealAccent),
-                  onPressed: () => _showItemDialog(document: document),
-                ),
-                IconButton(
-                  icon: Icon(Icons.delete, color: Colors.redAccent),
-                  onPressed: () => _deleteItem(document.id),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _showItemDialog({QueryDocumentSnapshot? document}) {
-    final TextEditingController nameController =
-        TextEditingController(text: document?.get('name') ?? '');
-    final TextEditingController quantityController = TextEditingController(
-        text: document?.get('quantity')?.toString() ?? '');
-
+  void _addItem() {
     showDialog(
       context: context,
       builder: (context) {
+        String name = '';
+        String quantity = '';
         return AlertDialog(
-          title: Text(document == null ? 'Add Item' : 'Edit Item'),
+          title: Text('Add Item'),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
               TextField(
-                controller: nameController,
                 decoration: InputDecoration(labelText: 'Item Name'),
+                onChanged: (value) => name = value,
               ),
               TextField(
-                controller: quantityController,
                 decoration: InputDecoration(labelText: 'Quantity'),
                 keyboardType: TextInputType.number,
+                onChanged: (value) => quantity = value,
               ),
             ],
           ),
           actions: [
             TextButton(
-              onPressed: () => Navigator.of(context).pop(),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
               child: Text('Cancel'),
             ),
             TextButton(
-              onPressed: () => _saveItem(
-                nameController.text,
-                quantityController.text,
-                document?.id,
-              ),
-              child: Text(document == null ? 'Add' : 'Update'),
+              onPressed: () async {
+                if (name.isNotEmpty && quantity.isNotEmpty) {
+                  try {
+                    await inventory
+                        .add({'name': name, 'quantity': int.parse(quantity)});
+                  } catch (e) {
+                    print("Error adding item: $e");
+                  }
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text('Add'),
             ),
           ],
         );
@@ -177,20 +214,55 @@ class _InventoryHomePageState extends State<InventoryHomePage> {
     );
   }
 
-  void _saveItem(String name, String quantity, String? id) async {
-    if (name.isNotEmpty && quantity.isNotEmpty) {
-      try {
-        int qty = int.parse(quantity);
-        if (id == null) {
-          await inventory.add({'name': name, 'quantity': qty});
-        } else {
-          await inventory.doc(id).update({'name': name, 'quantity': qty});
-        }
-      } catch (e) {
-        print("Error saving item: $e");
-      }
-    }
-    Navigator.of(context).pop();
+  void _editItem(QueryDocumentSnapshot document) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        String name = document['name'];
+        String quantity = document['quantity'].toString();
+        return AlertDialog(
+          title: Text('Edit Item'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: 'Item Name'),
+                onChanged: (value) => name = value,
+                controller: TextEditingController(text: name),
+              ),
+              TextField(
+                decoration: InputDecoration(labelText: 'Quantity'),
+                keyboardType: TextInputType.number,
+                onChanged: (value) => quantity = value,
+                controller: TextEditingController(text: quantity),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () async {
+                if (name.isNotEmpty && quantity.isNotEmpty) {
+                  try {
+                    await inventory.doc(document.id).update(
+                        {'name': name, 'quantity': int.parse(quantity)});
+                  } catch (e) {
+                    print("Error updating item: $e");
+                  }
+                }
+                Navigator.of(context).pop();
+              },
+              child: Text('Update'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   void _deleteItem(String id) async {
